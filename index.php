68a1ebff60bcfbb8704e87f89d877172f73791a4
aufgosh <42834590+aufgosh@users.noreply.github.com>
2021-09-21 11:46:43 +0200

    initial frontend commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include("./req/template/header.php") ?>
+
+  <body class="text-center">
+
+    <div class="container margin-auto">
+      <div class="inner-main center">
+        <h4>Willkommen bei</h3>
+        <a href="/login"><h1 class="heading-main">Coogle</h1></a>
+      </div>
+    </div>
+
+    <?php include("./req/template/footer.php") ?>
---
+++ req/template/header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Coogle</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/cover/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="./style/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="./style/styles.css" rel="stylesheet">
+  </head>
