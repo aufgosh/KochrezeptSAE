21c9cd27d817594db536196e4cd70fd521d4a50f
aufgosh <42834590+aufgosh@users.noreply.github.com>
2021-09-21 12:53:56 +0200

    Add files via upload

---
+++ req/template/sidebar.php
@@ -0,0 +1,40 @@
+<nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>Coogle</h3>
+            </div>
+
+            <ul class="list-unstyled components">
+                <p>Willkommen, User!</p>
+                <li class="active">
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
+                    <ul class="collapse list-unstyled" id="homeSubmenu">
+                        <li>
+                            <a href="#">Home 1</a>
+                        </li>
+                        <li>
+                            <a href="#">Home 2</a>
+                        </li>
+                        <li>
+                            <a href="#">Home 3</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Account</a>
+                    <ul class="collapse list-unstyled" id="pageSubmenu">
+                        <li>
+                            <a href="#">Einstellungen</a>
+                        </li>
+                        <li>
+                            <a href="#">Rezepte</a>
+                        </li>
+                        <li>
+                            <a href="#">Favoriten</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="/">Logout</a>
+                </li>
+            </ul>
+        </nav>
