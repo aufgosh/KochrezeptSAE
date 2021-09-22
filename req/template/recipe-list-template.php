4e64de62b8f726d0bcb6643929a5cee60031601b
aufgosh <42834590+aufgosh@users.noreply.github.com>
2021-09-22 09:57:25 +0200

    added recipe list template

---
+++ req/template/recipe-list-template.php
@@ -0,0 +1,11 @@
+<div class="line"></div>
+        <div>
+            <h3 class="recipe-h3">Geile Lasagne</h3>
+            <div class="row">
+                <div class="image-wrapper">
+                <img class="recipe-image col" src="https://img.chefkoch-cdn.de/rezepte/745721177147257/bilder/668335/crop-960x540/lasagne.jpg">
+                </div>
+                <p class="col">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <button class="btn btn-blue recipe-btn">Rezept anschauen</button>
+        </div>
