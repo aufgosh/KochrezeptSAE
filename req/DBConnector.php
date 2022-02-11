064ced994b82c2d0a17e492d8e583884b27d1cc1
Raeset1 <91118909+Raeset1@users.noreply.github.com>
2022-02-11 11:18:58 +0100

    Create DBConnector

---
+++ req/DBConnector.php
@@ -0,0 +1,35 @@
+<?php
+class DBConnector extends mysqli 
+{
+    private $user = "root";
+    private $pass = "";
+    private $dbHost = "localhost";
+
+    private static $instance = null;
+
+
+    /**
+     * @return DBConnector
+     */
+    public static function getConnector(){
+        if(!self::$instance instanceof self){
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
+
+    private function __construct(){
+        parent::__construct(this->dbHost,
+                            this->user
+                            this->pass);
+        if (mysqli_connect_error()){
+            exit('Verbindungsfehler('.mysqli_connect_errno().')'
+                    .mysqli_connect_error());
+        parent::set_charset('utf-8');
+        }
+    }
+    
+}
+
+
+?>
