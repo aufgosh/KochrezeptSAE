642ba2399666db50861ec7bedd1872323bd82525
Raeset1 <91118909+Raeset1@users.noreply.github.com>
2022-02-14 10:27:45 +0100

    Created new SQL Statement to Create the Database

---
+++ NEWDatabaseStatement.sql
@@ -0,0 +1,90 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema rezept
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema rezept
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `rezept` DEFAULT CHARACTER SET utf8 ;
+USE `rezept` ;
+
+-- -----------------------------------------------------
+-- Table `rezept`.`kategorie`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `rezept`.`kategorie` (
+  `idKategorie` INT(11) NOT NULL AUTO_INCREMENT,
+  `Kategorie` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`idKategorie`))
+
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `rezept`.`nutzer`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `rezept`.`nutzer` (
+  `NutzerID` INT(11) NOT NULL AUTO_INCREMENT,
+  `UserName` VARCHAR(45) NULL DEFAULT NULL,
+  `Passwd` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`NutzerID`))
+
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8;
+INSERT INTO Nutzer
+	(`NutzerID`, `UserName`, `Passwd`)
+VALUES
+	(1, "Lehrer", "d3f174c74ac93dcb59c40655c475db64690fdefb09e94a9cdf463f76517371bf");
+
+-- -----------------------------------------------------
+-- Table `rezept`.`gericht`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `rezept`.`gericht` (
+  `GerichtID` INT(11) NOT NULL AUTO_INCREMENT,
+  `Name` VARCHAR(45) NULL DEFAULT NULL,
+  `Zubereitungsanleitung` VARCHAR(45) NULL DEFAULT NULL,
+  `Bild` VARCHAR(45) NULL DEFAULT NULL,
+  `Anzahl Personen` VARCHAR(45) NULL DEFAULT NULL,
+  `kategorie_idKategorie` INT(11) NOT NULL,
+  `nutzer_NutzerID` INT(11) NOT NULL,
+  PRIMARY KEY (`GerichtID`),
+    FOREIGN KEY (`kategorie_idKategorie`)
+    REFERENCES `rezept`.`kategorie` (`idKategorie`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_gericht_nutzer1`
+    FOREIGN KEY (`nutzer_NutzerID`)
+    REFERENCES `rezept`.`nutzer` (`NutzerID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `rezept`.`zutaten`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `rezept`.`zutaten` (
+  `idZutaten` INT(11) NOT NULL,
+  `Name` VARCHAR(45) NULL DEFAULT NULL,
+  `Menge` INT(11) NULL DEFAULT NULL,
+  `gericht_GerichtID` INT(11) NOT NULL,
+  PRIMARY KEY (`idZutaten`),
+    FOREIGN KEY (`gericht_GerichtID`)
+    REFERENCES `rezept`.`gericht` (`GerichtID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+DEFAULT CHARACTER SET = utf8;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
