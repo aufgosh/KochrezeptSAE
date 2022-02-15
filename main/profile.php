ca3f54f9a65c44d240e061376ac2bb5139e97d68
aufgosh <42834590+aufgosh@users.noreply.github.com>
2022-02-15 11:19:59 +0100

    added recipes reated by user to user profile

--- main/profile.php
+++ main/profile.php
@@ -2,6 +2,7 @@
 require_once "Constants.php";
 require_once "../Autoloader.php";
 require_once(PATH_MAIN_HEADER_TEMPLATE);
+$dbAdatapter = \Core\DbAdapter::getInstance();
 ?>
 
 
@@ -17,7 +18,6 @@ require_once(PATH_MAIN_HEADER_TEMPLATE);
             <?php
             require_once(PATH_MAIN_INDEX_NAVBAR_TEMPLATE);
 
-            $dbAdatapter = \Core\DbAdapter::getInstance();
             $user = $dbAdatapter->getUser($_GET["id"]);
 
             ?>
@@ -30,8 +30,31 @@ require_once(PATH_MAIN_HEADER_TEMPLATE);
                     </div>
                     <h3><?php echo $user->getUsername(); ?></h3>
                 </div>
+                <br>
                 <div class="container profile-content">
+                <br>
+                <h2>Alle Retzepte von <?php echo $user->getUsername(); ?></h2>
+                <?php
+            $allRecp = \Core\DbAdapter::getInstance()->listRecipes($_GET['id']);
 
+            /** @var Recipe $recipe */
+            foreach ($allRecp as $recipe) {
+                #include(PATH_RECIPE_LIST_TEMPLATE);
+                echo sprintf("<div class='line'></div>
+                           <div>
+                                <h3 class='recipe-h3'>%s</h3>
+                                <div class='row'>
+                                    <div class='image-wrapper'>
+                                        <img class='recipe-image col'
+                                             src='https://img.chefkoch-cdn.de/rezepte/745721177147257/bilder/668335/crop-960x540/lasagne.jpg'>
+                                    </div>
+                                    <p class='col'>%s</p>
+                                </div>
+                                <br>
+                            <button class='btn btn-blue recipe-btn' onclick=\"window.location.href='../../main/recipe?id=%s'\">Rezept anschauen</button>
+                        </div>", $recipe->getRezeptName(), $recipe->getRezeptBeschreibung(), $recipe->getID());
+            }
+            ?>
                 </div>
             </div>
 
--- main/recipe.php
+++ main/recipe.php
@@ -1,50 +1,50 @@
-<?php 
-    require_once "Constants.php";
-    require_once "../Autoloader.php";
-    require_once(PATH_MAIN_HEADER_TEMPLATE);
+<?php
+require_once "Constants.php";
+require_once "../Autoloader.php";
+require_once(PATH_MAIN_HEADER_TEMPLATE);
+$dbAdatapter = \Core\DbAdapter::getInstance();
 ?>
 
 
-    <div class="wrapper">
-        <!-- Sidebar  -->
-        <?php 
-require_once(PATH_MAIN_SIDEBAR_TEMPLATE);
-?>
-<div class="container">
+<div class="wrapper">
+    <!-- Sidebar  -->
+    <?php
+    require_once(PATH_MAIN_SIDEBAR_TEMPLATE);
+    ?>
+    <div class="container">
         <!-- Page Content  -->
         <div id="content">
 
-<?php 
-require_once(PATH_MAIN_INDEX_NAVBAR_TEMPLATE);
+            <?php
+            require_once(PATH_MAIN_INDEX_NAVBAR_TEMPLATE);
 
-$dbAdatapter = \Core\DbAdapter::getInstance();
-$recipe = $dbAdatapter->getRecipeById($_GET["id"]);
-?>
+            $recipe = $dbAdatapter->getRecipeById($_GET["id"]);
+            ?>
 
             <h1><?= $recipe->getRezeptName(); ?></h1>
             <div class="recipe-container">
-            <div class="row">
-                <div class="col-md-4">
-                <img class="recipe-image" src="https://img.chefkoch-cdn.de/rezepte/745721177147257/bilder/668335/crop-960x540/lasagne.jpg">
-                <p>von <a href="profile?id=<?php echo $recipe->getNutzerID()?>"><?php $dbAdatapter->getUserForReceipt($recipe->getNutzerID()); ?></a></p>
+                <div class="row">
+                    <div class="col-md-4">
+                        <img class="recipe-image" src="https://img.chefkoch-cdn.de/rezepte/745721177147257/bilder/668335/crop-960x540/lasagne.jpg">
+                        <p>von <a href="profile?id=<?php echo $recipe->getNutzerID() ?>"><?php $dbAdatapter->getUserForReceipt($recipe->getNutzerID()); ?></a></p>
+                    </div>
+                    <div class="col-md-6">
+                        <h3>Rezept beschreibung</h3>
+                        <p><?= $recipe->getRezeptBeschreibung(); ?></p>
+                        <h3>Benötigte Zutaten</h3>
+                        <ul class="recipe-ul-styling">
+                            <?php
+                            $dbAdatapter->listIngredients($recipe->getZutaten());
+                            ?>
+                        </ul>
+                        <h3>Zubereitung</h3>
+                        <p><?= $recipe->getRezeptZubereitung(); ?></p>
+                    </div>
                 </div>
-                <div class="col-md-6">
-                    <h3>Rezept beschreibung</h3>
-                    <p><?= $recipe->getRezeptBeschreibung();?></p>
-                <h3>Benötigte Zutaten</h3>
-                <ul class="recipe-ul-styling">
-                <?php
-                $dbAdatapter->listIngredients($recipe->getZutaten());
-                ?>
-                </ul>
-                <h3>Zubereitung</h3>
-                <p><?= $recipe->getRezeptZubereitung();?></p>
-                </div>
-            </div>
             </div>
         </div>
     </div>
 </div>
 
-<?php 
-require_once(PATH_MAIN_FOOTER_TEMPLATE);  
+<?php
+require_once(PATH_MAIN_FOOTER_TEMPLATE);
