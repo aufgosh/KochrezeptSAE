786d57edff716d3aa3d442adfac7c96e02b425e3
Domikimo <75831292+Domikimo@users.noreply.github.com>
2022-02-16 22:23:40 +0100

    RegisterLogik.php aufgeräumt

--- Core/DbAdapter.php
+++ Core/DbAdapter.php
@@ -190,6 +190,96 @@ class DbAdapter
         }
         return $User;
     }
+
+    public function loginUser($username, $password) {
+        
+        // Check if the user is already logged in, if yes then redirect him to welcome page
+        if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+            header("location: /main/index");
+            exit;
+        }
+
+        // Processing form data when form is submitted
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            // Check if username is empty
+            if (empty(trim($_POST["username"]))) {
+                $username_err = "Please enter username.";
+            } else {
+                $username = trim($_POST["username"]);
+            }
+
+            // Check if password is empty and give an error if epmty
+            if (empty(trim($_POST["password"]))) {
+                $password_err = "Please enter your password.";
+            } else {
+                $password = trim($_POST["password"]);
+            }
+
+            // Validate credentials
+            if (empty($username_err) && empty($password_err)) {
+                // Prepare a select statement
+                $sql = "SELECT NutzerID, user, Password FROM nutzer WHERE User = ?";
+
+                if ($stmt = \Core\DbAdapter::getConnector()->prepare($sql)) {
+                    // Bind variables to the prepared statement as parameters
+                    mysqli_stmt_bind_param($stmt, "s", $username);
+
+                    // Set parameters and hash the password so it can be checked with the hash in the DB.
+                    $hashed_password = hash('sha256', $password);
+
+                    $password_from_ui = hash('sha256', $password);
+
+
+                    // Attempt to execute the prepared statement
+                    if (mysqli_stmt_execute($stmt)) {
+                        // Store result
+                        mysqli_stmt_store_result($stmt);
+
+                        // Check if username exists, if yes then verify password
+                        if (mysqli_stmt_num_rows($stmt) == 1) {
+                            // Bind result variables
+                            mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                            if (mysqli_stmt_fetch($stmt)) {
+
+
+                                //Check if password is correct 
+                                if ($password_from_ui == $hashed_password) {
+                                    // Password is correct, so start a new session
+                                    session_start();
+
+                                    // Store data in session variables
+                                    $_SESSION["loggedin"] = true;
+                                    $_SESSION["id"] = $id;
+                                    $_SESSION["username"] = $username;
+
+                                    // Redirect user to welcome page
+                                    header("location: /main/index");
+                                } else {
+                                    // Password is not valid, display a generic error message
+                                    $login_err = "Invalid username or password.";
+                                    echo $login_err;
+                                }
+                            }
+                        } else {
+                            // Username doesn't exist, display a generic error message
+                            $login_err = "Invalid username or password.";
+                            echo $login_err;
+                        }
+                    } else {
+                        echo "Oops! Something went wrong. Please try again later.";
+                    }
+
+                    // Close statement
+                    mysqli_stmt_close($stmt);
+                }
+            }
+
+            // Close connection
+            mysqli_close($link);
+        }
+    }
+
     public function registerUser($username, $password, $repeatpassword)
     {
         $user = new User();
@@ -231,4 +321,5 @@ class DbAdapter
         }
         $errorhandler->displayMessage($message, $errorbool);
     }
+
 }
--- account/loginLogik.php
+++ account/loginLogik.php
@@ -1,98 +1,16 @@
-<?php
+<?php 
 
-require_once "../Autoloader.php";
-
-
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-    header("location: /main/index");
-    exit;
-}
-
-// Include config file
 require_once "../req/sql.php";
-
 // Define variables and initialize with empty values
+//$dbAdatapter = \Core\DbAdapter::getInstance();
 $username = $password = "";
 $username_err = $password_err = $login_err = "";
+require_once "../Autoloader.php";
 
-// Processing form data when form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+use Entities\User;
 
-    // Check if username is empty
-    if (empty(trim($_POST["username"]))) {
-        $username_err = "Please enter username.";
-    } else {
-        $username = trim($_POST["username"]);
-    }
-
-    // Check if password is empty and give an error if epmty
-    if (empty(trim($_POST["password"]))) {
-        $password_err = "Please enter your password.";
-    } else {
-        $password = trim($_POST["password"]);
-    }
-
-    // Validate credentials
-    if (empty($username_err) && empty($password_err)) {
-        // Prepare a select statement
-        $sql = "SELECT NutzerID, user, Password FROM nutzer WHERE User = ?";
-
-        if ($stmt = \Core\DbAdapter::getConnector()->prepare($sql)) {
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $username);
-
-            // Set parameters and hash the password so it can be checked with the hash in the DB.
-            $hashed_password = hash('sha256', $password);
-
-            $password_from_ui = hash('sha256', $password);
-
-
-            // Attempt to execute the prepared statement
-            if (mysqli_stmt_execute($stmt)) {
-                // Store result
-                mysqli_stmt_store_result($stmt);
-
-                // Check if username exists, if yes then verify password
-                if (mysqli_stmt_num_rows($stmt) == 1) {
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if (mysqli_stmt_fetch($stmt)) {
-
-
-                        //Check if password is correct 
-                        if ($password_from_ui == $hashed_password) {
-                            // Password is correct, so start a new session
-                            session_start();
-
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;
-
-                            // Redirect user to welcome page
-                            header("location: /main/index");
-                        } else {
-                            // Password is not valid, display a generic error message
-                            $login_err = "Invalid username or password.";
-                            echo $login_err;
-                        }
-                    }
-                } else {
-                    // Username doesn't exist, display a generic error message
-                    $login_err = "Invalid username or password.";
-                    echo $login_err;
-                }
-            } else {
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }
-
-    // Close connection
-    mysqli_close($link);
+if (($_SERVER["REQUEST_METHOD"] == "POST")) {
+    $dbAdatapter = \Core\DbAdapter::getInstance();
+    $dbAdatapter->loginUser($_POST["username"], $_POST["password"]);
 }
-?>
+  
