cfe7ac04757ef3189b3e086ed7498b7737aec349
aufgosh <42834590+aufgosh@users.noreply.github.com>
2022-02-17 12:41:46 +0100

    added more errorhandling to register/login

--- account/loginLogik.php
+++ account/loginLogik.php
@@ -1,4 +1,5 @@
 <?php
+use Core\ErrorHandler;
 require_once "../Autoloader.php";
 
 if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
@@ -8,6 +9,18 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
 
 if (($_SERVER["REQUEST_METHOD"] == "POST")) {
     $dbAdatapter = \Core\DbAdapter::getInstance();
-    $dbAdatapter->loginUser($_POST["username"], $_POST["password"]);
+    $message = null;
+    $alert = null;
+
+    if (!empty($_POST['username']) && !empty($_POST["password"])) {
+        $dbAdatapter->loginUser($_POST["username"], $_POST["password"]);
+    } else {
+        $errorhandler = new ErrorHandler();
+        $alert = "Bitte das Formular ausfüllen.";
+        if ($alert != null) {
+            $message = $_POST["message"] = $alert;
+        }
+        $errorhandler->displayMessage($message, true);
+    }
 }
   
--- account/registerLogik.php
+++ account/registerLogik.php
@@ -1,7 +1,23 @@
 <?php
+
+use Core\ErrorHandler;
+
 require_once "../Autoloader.php";
 
 if (($_SERVER["REQUEST_METHOD"] == "POST")) {
     $dbAdatapter = \Core\DbAdapter::getInstance();
-    $dbAdatapter->registerUser($_POST["username"], $_POST["password"], $_POST["repeatpassword"]);
+    $message = null;
+    $alert = null;
+
+    if (!empty($_POST['username']) && !empty($_POST["password"]) && !empty($_POST['repeatpassword'])) {
+        $dbAdatapter->registerUser($_POST["username"], $_POST["password"], $_POST["repeatpassword"]);
+    } else {
+        $errorhandler = new ErrorHandler();
+        $alert = "Bitte das Formular ausfüllen.";
+        if ($alert != null) {
+            $message = $_POST["message"] = $alert;
+        }
+        $errorhandler->displayMessage($message, true);
+    }
+
 }
